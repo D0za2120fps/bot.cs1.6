a37a6ae3c844d0fd6749bb100e5a3c4fdf9419da
D0za2120fps <ilyateplak2013@gmail.com>
2025-11-28 01:53:34 +0300

    Refactor FTPHandler to simplify user addition

--- ftp_handler.py
+++ ftp_handler.py
@@ -1,28 +1,17 @@
-# utils/ftp_handler.py
 from ftplib import FTP
 from config import FTP_HOST, FTP_USER, FTP_PASS, USERS_INI_PATH
 
 class FTPHandler:
-    def __init__(self):
-        self.host = FTP_HOST
-        self.user = FTP_USER
-        self.password = FTP_PASS
-        self.path = USERS_INI_PATH
-
     def add_user(self, nick, password, flags):
-        ftp = FTP(self.host)
-        ftp.login(self.user, self.password)
-        # Скачать файл
-        lines = []
+        ftp = FTP(FTP_HOST)
+        ftp.login(FTP_USER, FTP_PASS)
         with open("users_temp.ini", "wb") as f:
-            ftp.retrbinary(f"RETR {self.path}", f.write)
+            ftp.retrbinary(f"RETR {USERS_INI_PATH}", f.write)
         with open("users_temp.ini", "r") as f:
             lines = f.readlines()
-        # Добавить нового пользователя
         lines.append(f'"{nick}" "{password}" "{flags}" "a"\n')
-        # Загрузить обратно
         with open("users_temp.ini", "w") as f:
             f.writelines(lines)
         with open("users_temp.ini", "rb") as f:
-            ftp.storbinary(f"STOR {self.path}", f)
+            ftp.storbinary(f"STOR {USERS_INI_PATH}", f)
         ftp.quit()
