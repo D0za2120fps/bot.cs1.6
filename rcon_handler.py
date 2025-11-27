fd5c4e19c7a741e73aa1693db0be2b1f32115d02
D0za2120fps <ilyateplak2013@gmail.com>
2025-11-28 01:53:46 +0300

    Update rcon_handler.py

--- rcon_handler.py
+++ rcon_handler.py
@@ -1,18 +1,12 @@
-# utils/rcon_handler.py
 import socket
-from config import RCON_PASS
+from config import RCON_HOST, RCON_PORT, RCON_PASS
 
 class RCONHandler:
-    def __init__(self, host='193.19.119.166', port=8888):
-        self.host = host
-        self.port = port
-        self.password = R2013dkwdm3
-
     def send_command(self, command):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            msg = f"\xff\xff\xff\xffrcon {self.password} {command}\n".encode()
-            s.sendto(msg, (self.host, self.port))
+            msg = f"\xff\xff\xff\xffrcon {RCON_PASS} {command}\n".encode()
+            s.sendto(msg, (RCON_HOST, RCON_PORT))
             s.close()
         except Exception as e:
             print("Ошибка RCON:", e)
